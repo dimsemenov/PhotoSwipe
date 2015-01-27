2b42123081f485e9bb14c14cf3fe906d0620bb34
Dmitry Semenov <diiiimaaaa@gmail.com>
2015-01-27 16:44:52 +0200

    build (js ui)

--- dist/photoswipe-ui-default.js
+++ dist/photoswipe-ui-default.js
@@ -1,4 +1,4 @@
-/*! PhotoSwipe Default UI - 4.0.5 - 2015-01-15
+/*! PhotoSwipe Default UI - 4.0.5 - 2015-01-27
 * http://photoswipe.com
 * Copyright (c) 2015 Dmitry Semenov; */
 /**
@@ -357,7 +357,7 @@ var PhotoSwipeUI_Default =
 						gap.bottom = bars.top; // if no caption, set size of bottom gap to size of top
 					}
 				} else {
-					gap.bottom = bars.bottom;
+					gap.bottom = bars.bottom === 'auto' ? 0 : bars.bottom;
 				}
 				
 				// height of top bar is static, no need to calculate it
