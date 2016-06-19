c33b1ce97f3caa4798d51e7d8d38c26b2eec222e
Jeffrey Scott French <jeffreyscottfrench@gmail.com>
2016-06-19 18:08:35 -0400

    Updated initial dist files

--- dist/photoswipe-ui-default.js
+++ dist/photoswipe-ui-default.js
@@ -1,6 +1,6 @@
-/*! PhotoSwipe Default UI - 4.1.1 - 2015-12-24
+/*! PhotoSwipe Default UI - 4.1.1 - 2016-06-19
 * http://photoswipe.com
-* Copyright (c) 2015 Dmitry Semenov; */
+* Copyright (c) 2016 Dmitry Semenov; */
 /**
 *
 * UI on top of main sliding area (caption, arrows, close button, etc.).
--- dist/photoswipe.js
+++ dist/photoswipe.js
@@ -1,6 +1,6 @@
-/*! PhotoSwipe - v4.1.1 - 2015-12-24
+/*! PhotoSwipe - v4.1.1 - 2016-06-19
 * http://photoswipe.com
-* Copyright (c) 2015 Dmitry Semenov; */
+* Copyright (c) 2016 Dmitry Semenov; */
 (function (root, factory) { 
 	if (typeof define === 'function' && define.amd) {
 		define(factory);
