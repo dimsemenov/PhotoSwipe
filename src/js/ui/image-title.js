173fb632ee31ef55ebb0335f95026e0b6c363259
Bruno Silva <bruno.silva@ada.ac.uk>
2021-06-25 09:50:40 +0100

    Fixed linting issues

--- src/js/ui/image-title.js
+++ src/js/ui/image-title.js
@@ -1,11 +1,11 @@
 export const imageTitle = {
-    name: 'title',
-    title: 'Title',
-    class: 'image--title',
-    order: 6,
-    onInit: (titleElement, pswp) => {
-        pswp.on('change', () => {
-            titleElement.innerHTML = pswp.currItemData.alt;
-        });
-    }
+  name: 'title',
+  title: 'Title',
+  class: 'image--title',
+  order: 6,
+  onInit: (titleElement, pswp) => {
+    pswp.on('change', () => {
+      titleElement.innerHTML = pswp.currItemData.alt;
+    });
+  }
 };
