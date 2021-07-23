4bb6937716253d4d730790cf6ccbeb1e6931b812
Dmitry Semenov <diiiimaaaa@gmail.com>
2021-07-23 08:26:59 +0300

    Add beforeZoomTo event

--- src/js/slide/slide.js
+++ src/js/slide/slide.js
@@ -405,6 +405,10 @@ class Slide {
       return;
     }
 
+    pswp.dispatch('beforeZoomTo', {
+      destZoomLevel, centerPoint, transitionDuration
+    });
+
     // stop all pan and zoom transitions
     pswp.animations.stopAllPan();
 
