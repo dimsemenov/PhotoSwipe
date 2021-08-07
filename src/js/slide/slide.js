5a0da8920592cdf3e88a6e23bd75c182bbd6619a
Dmitry Semenov <diiiimaaaa@gmail.com>
2021-08-07 07:46:46 +0300

    Fix: flashing in Safari and FF when switching slides

--- src/js/slide/slide.js
+++ src/js/slide/slide.js
@@ -141,9 +141,7 @@ class Slide {
   preload() {
     this.image = createElement('pswp__img', 'img');
 
-    // Not adding `async`,
-    // as it causes flash of image after it's loaded in Safari
-    // this.image.decoding = 'async';
+    
 
     // may update sizes attribute
     this._updateImagesSize();
@@ -155,6 +153,17 @@ class Slide {
     this.image.src = this.data.src;
 
     this.image.alt = this.data.alt || '';
+
+    // Not adding `async`,
+    // as it causes flash of image after it's loaded in Safari
+    // this.image.decoding = 'async';
+
+    // Using decode() to force nearby images to render:
+    // even though nearby images are in DOM and not hidden via display:none,
+    // Safari and FF still do not render them as they're offscreen. This helps:
+    if (!this.isActive && ('decode' in this.image)) {
+      this.image.decode();
+    }
 
     this.pswp.lazyLoader.addRecent(this.index);
   }
