e4403aa7b84aead45bae66b5a2c012b3fa3148d1
Dmitry Semenov <diiiimaaaa@gmail.com>
2022-03-26 14:56:42 +0200

    Update index.js

--- demo-docs-website/src/pages/index.js
+++ demo-docs-website/src/pages/index.js
@@ -111,8 +111,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={siteConfig.title}
+      description="Open-source JavaScript image gallery and lightbox.">
       <HomepageHeader />
       <WhatsNew />
     </Layout>
